feec09270a2a5e963705806c4b4678196c23a1cd
pk2733 <84867963+pk2733@users.noreply.github.com>
2021-06-23 01:03:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,109 +1,161 @@
-import os
 
-os.system("cls")
+board = {1: ' ', 2: ' ', 3: ' ',
+         4: ' ', 5: ' ', 6: ' ',
+         7: ' ', 8: ' ', 9: ' '}
 
 
-class Board:
-    def __init__(self):
-        self.cells = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+def printBoard(board_game):
+    print("Positions")
+    print("1, 2, 3 ")
+    print("4, 5, 6 ")
+    print("7, 8, 9 ")
+    print("\n")
+    print(board_game[1] + ' | ' + board_game[2] + ' | ' + board_game[3])
+    print('---------')
+    print(board_game[4] + ' | ' + board_game[5] + ' | ' + board_game[6])
+    print('---------')
+    print(board_game[7] + ' | ' + board_game[8] + ' | ' + board_game[9])
 
-    def display(self):
-        print(" %s | %s | %s " % (self.cells[1], self.cells[2], self.cells[3]))
-        print("--------------")
-        print(" %s | %s | %s " % (self.cells[4], self.cells[5], self.cells[6]))
-        print("--------------")
-        print(" %s | %s | %s " % (self.cells[7], self.cells[8], self.cells[9]))
-        print("--------------")
 
-    def update_cell(self, cell_no, player):
-        if self.cells[cell_no] == " ":
-            self.cells[cell_no] = player
-
-    def is_winner(self, player):
-        for combo in [[1, 2, 3], [3, 4, 5], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]:
-            result = True
-            for cell_no in combo:
-                if self.cells[cell_no] != player:
-                    result = False
-
-            if result:
-                return True
-
+def spaceIsFree(position):
+    if board[position] == ' ':
+        return True
+    else:
         return False
 
-    def is_tie(self):
-        used_cells = 0
-        for cell in self.cells:
-            if cell != " ":
-                used_cells += 1
-        if used_cells == 9:
-            return True
-        else:
+
+def insertLetter(letter, position):
+    if spaceIsFree(position):
+        board[position] = letter
+        printBoard(board)
+        if checkDraw():
+            print("Draw!")
+            exit()
+        if checkForWin():
+            if letter == 'X':
+                print("Computer wins!")
+                exit()
+            else:
+                print("Player wins!")
+                exit()
+
+        return
+
+    else:
+        print("Can't insert there!")
+        position = int(input("Please enter new position:  "))
+        insertLetter(letter, position)
+        return
+
+
+def checkForWin():
+    if board[1] == board[2] and board[1] == board[3] and board[1] != ' ':
+        return True
+    elif board[4] == board[5] and board[4] == board[6] and board[4] != ' ':
+        return True
+    elif board[7] == board[8] and board[7] == board[9] and board[7] != ' ':
+        return True
+    elif board[1] == board[4] and board[1] == board[7] and board[1] != ' ':
+        return True
+    elif board[2] == board[5] and board[2] == board[8] and board[2] != ' ':
+        return True
+    elif board[3] == board[6] and board[3] == board[9] and board[3] != ' ':
+        return True
+    elif board[1] == board[5] and board[1] == board[9] and board[1] != ' ':
+        return True
+    elif board[7] == board[5] and board[7] == board[3] and board[7] != ' ':
+        return True
+    else:
+        return False
+
+
+def checkWhichMarkWon(mark):
+    if board[1] == board[2] and board[1] == board[3] and board[1] == mark:
+        return True
+    elif board[4] == board[5] and board[4] == board[6] and board[4] == mark:
+        return True
+    elif board[7] == board[8] and board[7] == board[9] and board[7] == mark:
+        return True
+    elif board[1] == board[4] and board[1] == board[7] and board[1] == mark:
+        return True
+    elif board[2] == board[5] and board[2] == board[8] and board[2] == mark:
+        return True
+    elif board[3] == board[6] and board[3] == board[9] and board[3] == mark:
+        return True
+    elif board[1] == board[5] and board[1] == board[9] and board[1] == mark:
+        return True
+    elif board[7] == board[5] and board[7] == board[3] and board[7] == mark:
+        return True
+    else:
+        return False
+
+
+def checkDraw():
+    for key in board.keys():
+        if board[key] == ' ':
             return False
-
-    def reset(self):
-        self.cells = [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
+    return True
 
 
-board = Board()
+def playerMove():
+    print("\n")
+    position = int(input("Enter the position for player 'O':  "))
+    insertLetter(player, position)
+    return
 
 
-def print_header():
-    print("Welcome to Tic-Tac-Toe\n")
+def compMove():
+    best_score = -100
+    best_move = 0
+    for key in board.keys():
+        if board[key] == ' ':
+            board[key] = computer
+            score = minimax(board, 0, False)
+            board[key] = ' '
+            if score > best_score:
+                best_score = score
+                best_move = key
+
+    insertLetter(computer, best_move)
+    return
 
 
-def refresh_screen():
-    os.system("cls")
-    print_header()
-    board.display()
+def minimax(board_game, depth, isMaximizing):
+    if checkWhichMarkWon(computer):
+        return 1
+    elif checkWhichMarkWon(player):
+        return -1
+    elif checkDraw():
+        return 0
+
+    if isMaximizing:
+        best_score = -100
+        for key in board_game.keys():
+            if board_game[key] == ' ':
+                board_game[key] = computer
+                score = minimax(board_game, depth, False)
+                board_game[key] = ' '
+                if score > best_score:
+                    best_score = score
+        return best_score
+
+    else:
+        best_score = 100
+        for key in board_game.keys():
+            if board_game[key] == ' ':
+                board_game[key] = player
+                score = minimax(board_game, depth, True)
+                board_game[key] = ' '
+                if score < best_score:
+                    best_score = score
+        return best_score
 
 
-refresh_screen()
+printBoard(board)
+player = 'O'
+computer = 'X'
 
-while True:
-    refresh_screen()
-    x_choice = int(input("\nX) Please choose 1 - 9. > "))
 
-    board.update_cell(x_choice, "X")
-
-    refresh_screen()
-
-    if board.is_winner("X"):
-        print("\nX wins\n")
-        play_again = input("Would you like to play again? (y/n) > ")
-        if play_again == "y":
-            board.reset()
-            continue
-        else:
-            break
-
-    if board.is_tie():
-        print("\nTie game\n")
-        play_again = input("Would you like to play again? (y/n) > ")
-        if play_again == "y":
-            board.reset()
-            continue
-        else:
-            break
-
-    o_choice = int(input("\nO) Please choose 1 - 9. > "))
-
-    board.update_cell(o_choice, "O")
-
-    if board.is_winner("O"):
-        print("\nO wins\n")
-        play_again = input("Would you like to play again? (y/n) > ")
-        if play_again == "y":
-            board.reset()
-            continue
-        else:
-            break
-
-    if board.is_tie():
-        print("\nTie game\n")
-        play_again = input("Would you like to play again? (y/n) > ")
-        if play_again == "y":
-            board.reset()
-            continue
-        else:
-            break
+while not checkForWin():
+    compMove()
+    playerMove()
